2180b558171fa88a61c1a0374d0c3f336eeec966
vky-fsd <vk206vignesh@gmail.com>
2023-10-27 13:54:24 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const passwordResetRoutes = require("./routes/passwordReset");
+const urlRoutes = require('./routes/urlRoutes');
+
+
+// database connection
+connection();
+
+// middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/password-reset", passwordResetRoutes);
+app.use('/', urlRoutes);
+app.use(express.static('public'));
+
+const port = 3000;
+app.listen(port, console.log(`Listening on port ${port}...`));
